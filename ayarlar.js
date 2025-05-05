26208ec026515f2e82c8fefc43bfe3ded7d441b3
StormV99 <toprak122.fo@gmail.com>
2025-05-05 20:43:26 +0300

    Update ayarlar.js

--- ayarlar.js
+++ ayarlar.js
@@ -1,9 +1,9 @@
 module.exports = {
-  prefix: "BOT PREFİX",
-  sahip: "Senin ID'n",
+  prefix: "nrv!",
+  sahip: "959492752645701703",
   color: 0x0074ff,
-  proseries: "PRO SERİES ROL ID",
-  proserieskanal: "PRO SERİES KANAL ID",
-  emoji: "EMOJİ",
-  token: "BOT TOKEN"
+  proseries: "1360576981351272570",
+  proserieskanal: "1360577301158432768",
+  emoji: "1369006016200573049",
+  token: "MTM2OTAwNjE1NTcwNzMxODQ2NA.Gbx53Q.eraUoB9ds0cNzk1JWbWhVAgnVafiELAQqZRhGg"
 }
